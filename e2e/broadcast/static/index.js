a036852ba82cf335c2b08541ab4662026b160a3f
William <gopherx@protonmail.ch>
2021-04-03 10:02:16 +0800

    Update static js

--- e2e/broadcast/static/index.js
+++ e2e/broadcast/static/index.js
@@ -9,7 +9,7 @@
     });
 
     let log = (msg) => {
-      document.getElementById(`log${i}`).innerHTML += msg + "<br>";
+      document.getElementById(`log${i + 1}`).innerHTML += msg + "<br>";
     };
 
     pc.ontrack = function (event) {
@@ -28,30 +28,27 @@
     pc.createOffer()
       .then((offer) => {
         pc.setLocalDescription(offer);
-        console.log(`Sending offer ${i}: ${offer}`);
 
         sdp = {
           id: "fa955cc6881b4b45b49ffbf2d81e7223",
           track_source: i,
           sdp: offer,
         };
+        console.log(`Sending offer ${i}: ${JSON.stringify(sdp)}`);
 
-        return fetch("http://localhost:8080/v1/broadcast/signal", {
+        return fetch("/v1/broadcast/signal", {
           method: "post",
           headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
-            'Access-Control-Allow-Origin':'*'
           },
-          mode: 'no-cors',
           body: JSON.stringify(sdp),
         });
       })
       .then((res) => res.json())
       .then((res) => {
-        console.log(`Received answer ${i}: ${res}`);
-
         pc.setRemoteDescription(res);
+        console.log(`Received answer ${i}: ${JSON.stringify(res)}`);
       })
       .catch(log);
   }
