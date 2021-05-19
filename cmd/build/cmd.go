2f3312e8a109aeeaf9b56845e0fb5cae46150177
William <gopherx@protonmail.ch>
2021-05-19 23:39:11 +0800

    build: add build info

---
+++ cmd/build/cmd.go
@@ -0,0 +1,30 @@
+package build
+
+import (
+	"fmt"
+
+	"github.com/urfave/cli/v2"
+)
+
+var (
+	Branch    string
+	Version   string
+	Revision  string
+	BuildUser string
+	BuildDate string
+)
+
+func Command() *cli.Command {
+	return &cli.Command{
+		Name:  "info",
+		Usage: "info displays build infomation of this binary",
+		Action: func(c *cli.Context) error {
+			fmt.Printf(`Branch:		%s
+Version:	%s
+Revision:	%s
+BuildUser:	%s
+BuildDate:	%s`, Branch, Version, Revision, BuildUser, BuildDate)
+			return nil
+		},
+	}
+}
