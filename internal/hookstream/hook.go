cce9b85cb5bc8452213715488e505497cefcb7a8
William <gopherx@protonmail.ch>
2021-05-26 21:00:32 +0800

    hookstream: filter state of disconnection

--- internal/hookstream/hook.go
+++ internal/hookstream/hook.go
@@ -9,6 +9,7 @@ import (
 	mqttclient "github.com/SB-IM/mqtt-client"
 	pb "github.com/SB-IM/pb/signal"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/pion/webrtc/v3"
 	"github.com/rs/zerolog/log"
 )
 
@@ -36,7 +37,17 @@ func Exec(ctx context.Context, config ConfigOptions) (errCh chan error) {
 	errCh = make(chan error, 1)
 
 	topic := config.HookStreamTopicPrefix + "/" + config.UUID + "/" + strconv.Itoa(int(pb.TrackSource_DRONE))
-	t := client.Subscribe(topic, byte(config.Qos), func(c mqtt.Client, _ mqtt.Message) {
+	t := client.Subscribe(topic, byte(config.Qos), func(c mqtt.Client, m mqtt.Message) {
+		payload, err := strconv.Atoi(string(m.Payload()))
+		if err != nil {
+			logger.Err(err).Bytes("payload", m.Payload()).Msg("invalid message")
+			return
+		}
+		state := webrtc.ICEConnectionState(payload)
+		if state == webrtc.ICEConnectionStateDisconnected {
+			return
+		}
+
 		time.Sleep(config.WaitTimeout)
 		logger.Info().Dur("wait", config.WaitTimeout).Msg("wait for a while")
 
